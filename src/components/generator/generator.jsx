9aa69b167506827d071f7dd71c36503185e54e18
perpl777 <mllw327@gmail.com>
2024-04-11 20:27:33 +0700

    changed with useref

--- src/components/generator/generator.jsx
+++ src/components/generator/generator.jsx
@@ -1,20 +1,19 @@
-
 import React, { useState, useRef, useEffect } from 'react';
 import styles from './generator.module.css';
 
 function ShadowGenerator() {
-    const [boxShadow, setBorderShadow] = useState({
+    const [boxShadow, setBoxShadow] = useState({
         horizontal: 0,
         vertical: 0,
         blur: 0,
         spread: 0,
-        color: 'rgba(0, 0, 0, 0.5)',
+        color: 'black',
         inset: false
     });
-    const [boxResult, setBorderResult] = useState('');
+
+    const [boxResult, setBoxResult] = useState('');
 
     const squareRef = useRef(null);
-
 
     const generateBorder = () => {
         squareRef.current.style.boxShadow = `
@@ -25,41 +24,64 @@ function ShadowGenerator() {
             ${boxShadow.color} 
             ${boxShadow.inset ? 'inset' : ''}`;
     }
-    
 
     useEffect(() => {
         generateBorder();
-        setBorderResult(`
+        setBoxResult(`
             box-shadow: ${boxShadow.horizontal}px ${boxShadow.vertical}px ${boxShadow.blur}px ${boxShadow.spread}px ${boxShadow.color} ${boxShadow.inset ? 'inset' : ''};`);
     }, [boxShadow]);
+
+    const handleHorizontalChange = (e) => {
+        setBoxShadow({ ...boxShadow, horizontal: parseInt(e.target.value) });
+    }
+
+    const handleVerticalChange = (e) => {
+        setBoxShadow({ ...boxShadow, vertical: parseInt(e.target.value) });
+    }
+
+    const handleBlurChange = (e) => {
+        setBoxShadow({ ...boxShadow, blur: parseInt(e.target.value) });
+    }
+
+    const handleSpreadChange = (e) => {
+        setBoxShadow({ ...boxShadow, spread: parseInt(e.target.value) });
+    }
+
+    const handleColorChange = (e) => {
+        setBoxShadow({ ...boxShadow, color: e.target.value });
+    }
+
+    const handleInsetChange = (e) => {
+        setBoxShadow({ ...boxShadow, inset: e.target.checked });
+    }
 
     return (
         <div className={styles.container}>
             <div className={styles.square} ref={squareRef}></div>
-    
-                <div className={styles.shadow}>
-                    <div className={styles.shadowInputs}>
-                        <label>Horizontal
-                            <input type="range" min="-100" max="100" value={boxShadow.horizontal} onChange={(e) => setBorderShadow({ ...boxShadow, horizontal: parseInt(e.target.value) })} />
-                        </label>
-                        <label>Vertical
-                            <input type="range" min="-100" max="100" value={boxShadow.vertical} onChange={(e) => setBorderShadow({ ...boxShadow, vertical: parseInt(e.target.value) })} />
-                        </label>
-                        <label>Blur
-                            <input type="range" min="0" max="100" value={boxShadow.blur} onChange={(e) => setBorderShadow({ ...boxShadow, blur: parseInt(e.target.value) })} />
-                        </label>
-                        <label>Spread
-                            <input type="range" min="0" max="100" value={boxShadow.spread} onChange={(e) => setBorderShadow({ ...boxShadow, spread: parseInt(e.target.value) })} />
-                        </label>
-                        <label>Color
-                            <input type="color" value={boxShadow.color} onChange={(e) => setBorderShadow({ ...boxShadow, color: e.target.value })} />
-                        </label>
-                        <label> Inset
-                            <input type="checkbox" checked={boxShadow.inset} onChange={() => setBorderShadow({ ...boxShadow, inset: !boxShadow.inset })} />
-                        </label>
-                    </div>
+
+            <div className={styles.shadow}>
+                <div className={styles.shadowInputs}>
+                    <label>Horizontal
+                        <input type="range" min="-100" max="100" value={boxShadow.horizontal} onChange={handleHorizontalChange} />
+                    </label>
+                    <label>Vertical
+                        <input type="range" min="-100" max="100" value={boxShadow.vertical} onChange={handleVerticalChange} />
+                    </label>
+                    <label>Blur
+                        <input type="range" min="0" max="100" value={boxShadow.blur} onChange={handleBlurChange} />
+                    </label>
+                    <label>Spread
+                        <input type="range" min="0" max="100" value={boxShadow.spread} onChange={handleSpreadChange} />
+                    </label>
+                    <label>Color
+                        <input type="color" value={boxShadow.color} onChange={handleColorChange} />
+                    </label>
+                    <label> Inset
+                        <input type="checkbox" checked={boxShadow.inset} onChange={handleInsetChange} />
+                    </label>
                 </div>
-    
+            </div>
+
             <div className={styles.result}>
                 <textarea className={styles.textarea} value={boxResult}></textarea>
                 <button className={styles.copyBtn} onClick={() => navigator.clipboard.writeText(boxResult)}>Copy</button>
